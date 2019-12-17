d529988bbd6e75a7706274294d354664c2246b00
Jack <jacktao998@163.com>
2019-12-17 13:24:54 +0800

    首页路由配置

---
+++ src/views/home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Home
+        <h1>哈哈哈哈哈</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
