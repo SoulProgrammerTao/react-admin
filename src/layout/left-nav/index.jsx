81a72e967141e7e75760d0f71955aa4e782a8abe
Jack <jacktao998@163.com>
2019-12-26 15:46:45 +0800

    菜单栏优化

--- src/layout/left-nav/index.jsx
+++ src/layout/left-nav/index.jsx
@@ -3,8 +3,37 @@ import './index.scss'
 import logo from "../../assets/images/logo.png";
 import { Menu, Icon } from 'antd';
 import { Link } from "react-router-dom";
+import menuConfig from "../../config/menuConfig";
 const { SubMenu } = Menu;
 class LeftNav extends Component {
+  getMenuNodes (menuList) {
+    return menuList.map(item => {
+      if (item.children) {
+        return (
+          <SubMenu
+            key={item.key}
+            title={
+              <span>
+                <Icon type={item.icon} />
+                <span>{item.title}</span>
+              </span>
+            }
+          >
+            {this.getMenuNodes(item.children)}
+          </SubMenu>
+        )
+      } else {
+        return (
+          <Menu.Item key={item.key}>
+            <Link to={item.key}>
+              <Icon type={item.icon} />
+              <span>{item.title}</span>
+            </Link>
+          </Menu.Item>
+        )
+      }
+    })
+  }
   render() {
     return (
       <div className="left-nav">
@@ -16,80 +45,7 @@ class LeftNav extends Component {
         mode="inline"
         theme="dark"
         >
-          <Menu.Item key="1">
-            <Link to="/home">
-              <Icon type="home" />
-              <span>首页</span>
-            </Link>
-          </Menu.Item>
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <Icon type="heart" />
-                <span>商品</span>
-              </span>
-            }
-          >
-            <Menu.Item key="2">
-              <Link to="/category">
-                <Icon type="heart" />
-                <span>品类管理</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Link to="/product">
-                <Icon type="heart" />
-                <span>商品管理</span>
-              </Link>
-            </Menu.Item>
-          </SubMenu>
-          <Menu.Item key="4">
-            <Link to="/user">
-              <Icon type="heart" />
-              <span>用户管理</span>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="5">
-            <Link to="/role">
-              <Icon type="heart" />
-              <span>角色管理</span>
-            </Link>
-          </Menu.Item>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <Icon type="heart" />
-                <span>图形图表</span>
-              </span>
-            }
-          >
-            <Menu.Item key="6">
-              <Link to="/bar">
-                <Icon type="heart" />
-                <span>柱形图</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="7">
-              <Link to="/line">
-                <Icon type="heart" />
-                <span>折线图</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="8">
-              <Link to="/pie">
-                <Icon type="heart" />
-                <span>饼图</span>
-              </Link>
-            </Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9">
-            <Link to="/order">
-              <Icon type="heart" />
-              <span>订单管理</span>
-            </Link>
-          </Menu.Item>
+          {this.getMenuNodes(menuConfig)}
         </Menu>
       </div>
     );
